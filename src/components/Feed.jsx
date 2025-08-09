54de8fbdf7ac93461f0e27268ceadbfa9d4488c0
manzoor elahi <manzoorelahi81092001@gmail.com>
2025-08-09 09:05:05 +0530

    feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import BASE_URL from '../../utils/constant';
+import { addFeed } from '../../utils/feedSlice';
+import FeedCard from './FeedCard';
 
 const Feed = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const feed = useSelector((store) => store.feed);
+    const dispatch = useDispatch();
 
-export default Feed
+    const getFeed = async () => {
+        // Agar feed already hai to API mat call karo
+        if (feed && feed.length > 0) return;
+
+        try {
+            const res = await axios.get(BASE_URL + "/feed", {
+                withCredentials: true
+            });
+            
+            dispatch(addFeed(res?.data?.data));
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getFeed();
+    }, []);
+
+     // Ye redux ka data show karega jab update hoga
+
+    return (
+  feed && feed.length > 0 && (
+    <div>
+      <FeedCard user={feed[0]} />
+    </div>
+  
+)
+    )
+
+    
+};
+
+export default Feed;
---
+++ src/components/FeedCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const FeedCard = ({user}) => {
+    const {firstName, lastName, skills, gender, age, photoUrl, about} = user;
+  return (
+     <div className=" flex items-center justify-center bg-[#e0e5ec] mt-[60px]">
+      <div className="bg-[#e0e5ec] rounded-2xl shadow-[10px_10px_20px_#a3b1c6,-10px_-10px_20px_#ffffff]  w-70 h-100 text-center">
+        <img src={user.photoUrl} alt='photo'/>
+        <h1 >{firstName+" "+lastName}</h1>
+        {age && gender &&<p>{age+","+gender}</p>}
+        <p className='text-start'>{about}</p>
+        <div className='flex justify-center gap-x-8 pt-[20px]'>
+          <button className="w-1/3 py-2 rounded-xl bg-blue-400 font-medium 
+             shadow-[10px_10px_20px_#a3b1c6,-10px_-10px_20px_#ffffff] 
+            hover:scale-105 transition duration-200 cursor-pointer">
+          Ignore
+        </button>
+        <button  className='w-1/3 py-2 rounded-xl bg-pink-500 font-medium 
+        shadow-[10px_10px_20px_#a3b1c6,-10px_-10px_20px_#ffffff] 
+        hover:scale-105 transition duration-200 cursor-pointer' >Interested</button>
+        
+        </div>
+      </div>
+    
+    </div>
+  )
+}
+
+export default FeedCard
